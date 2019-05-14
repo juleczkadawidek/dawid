4b597d618f4a01b37443c9e59d5b265641fcfdb1
PStawowczyk <alexis135832@wp.pl>
2019-05-14 13:49:56 +0200

    Dodalem projekt ze skryptem

---
+++ Project5/Project5/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char nazwa[20];
+
+void pio()
+{
+
+	cout << "Wprowadz wyraz a ja wyswietle pierwsza i ostatnia literke kotku: ";
+	cin >> nazwa;
+	cout << "To jest pierwsza litera " << nazwa[0];
+	cout<< "Ostatnia litera to: " 
+
+
+}
+
+
+int main()
+
+
+{
+	pio();
+	 
+
+
+	system("pause");
+	return 0;
+	
+}
+
